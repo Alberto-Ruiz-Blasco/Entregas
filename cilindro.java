2c589b19b1c2a3c033d802197ce0273b95468ead
Alberto-Ruiz-Blasco <albruibla@alu.edu.gva.es>
2025-01-21 09:12:15 +0100

    Create cilindro.java

---
+++ cilindro.java
@@ -0,0 +1,21 @@
+public class cilindro extends circulo{
+
+    double altura;
+
+    public cilindro(double radio, double altura) {
+        super(radio);
+        if (altura<=0){
+            this.altura=0;
+        } else {
+            this.altura = altura;
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getVolume(){
+        return getArea() * altura;
+    }
+}

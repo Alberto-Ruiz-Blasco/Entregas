895a4548bc2cb2c291c181cffa26434ed7336379
Alberto-Ruiz-Blasco <albruibla@alu.edu.gva.es>
2025-01-21 09:11:44 +0100

    Create circulo.java

---
+++ circulo.java
@@ -0,0 +1,20 @@
+public class circulo {
+
+    double radio;
+
+    public circulo(double radio) {
+        if (radio<0){
+            this.radio=0;
+        } else {
+            this.radio=radio;
+        }
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double getArea(){
+        return radio * radio * Math.PI;
+    }
+}

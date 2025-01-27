9a267a023614e576def69ef63dea1b399bdf93d5
Alberto-Ruiz-Blasco <albruibla@alu.edu.gva.es>
2025-01-27 08:08:42 +0100

    Create Esfera.java

---
+++ forma.docs/Esfera.java
@@ -0,0 +1,16 @@
+package Tarea3_Ej4;
+
+public class Esfera extends Forma{
+
+    protected double radio;
+
+    public Esfera(double radio) {
+        super("Esfera");
+        this.radio = radio;
+    }
+
+    @Override
+    public double area(){
+        return 4 * Math.PI * radio *radio;
+    }
+}

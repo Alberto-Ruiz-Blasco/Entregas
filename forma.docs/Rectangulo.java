4e99d340c85175d2b48bb5ae3af57a821d0f69a7
Alberto-Ruiz-Blasco <albruibla@alu.edu.gva.es>
2025-01-27 08:10:22 +0100

    Create Rectangulo.java

---
+++ forma.docs/Rectangulo.java
@@ -0,0 +1,18 @@
+package Tarea3_Ej4;
+
+public class Rectangulo extends Forma{
+
+    protected double longitud;
+    protected double ancho;
+
+    public Rectangulo(String nombre, double longitud, double ancho) {
+        super(nombre);
+        this.longitud = longitud;
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double area(){
+        return longitud * ancho;
+    }
+}
